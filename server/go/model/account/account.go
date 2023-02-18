1d2b3eab3f3fb268fbc6553cc9fde31234baef59
chris.han <namilhan@gmail.com>
2023-02-18 10:46:37 +0900

    refactoring go server

--- server/go/model/account/account.go
+++ server/go/model/account/account.go
@@ -2,6 +2,8 @@ package account
 
 import (
 	"context"
+	"time"
+
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
@@ -10,24 +12,25 @@ type Server struct {
 }
 
 func (s *Server) GetAccount(ctx context.Context, in *GetAccountRequest) (*GetAccountResponse, error) {
-	return &GetAccountResponse{
-		Account: &Account{
-			Id:        in.Id,
-			Name:      "Chris",
-			Email:     "namilhan@gmail.com",
-			Phone:     "010-1234-5678",
-			Avatar:    "https://avatars0.githubusercontent.com/u/16812446?s=460&v=4",
-			CreatedAt: timestamppb.Now(),
-			Address: &Address{
-				Street: "gangnam",
-				City:   "seoul",
-				State:  "korea",
-				Zip:    "12345",
-			},
+	account := &Account{
+		Id:        in.Id,
+		Name:      "Chris",
+		Email:     "namilhan@gmail.com",
+		Phone:     "010-1234-5678",
+		Avatar:    "https://avatars0.githubusercontent.com/u/16812446?s=460&v=4",
+		CreatedAt: timestamppb.New(time.Now()),
+		Address: &Address{
+			Street: "gangnam",
+			City:   "seoul",
+			State:  "korea",
+			Zip:    "12345",
 		},
-	}, nil
+	}
+
+	return &GetAccountResponse{Account: account}, nil
 }
 
 func (s *Server) CreateAccount(ctx context.Context, in *CreateAccountRequest) (*CreateAccountResponse, error) {
+	// TODO: Implement the account creation logic
 	return nil, nil
 }
